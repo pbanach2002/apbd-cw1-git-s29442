aea8953f9c1b1ab3fc44c92d5459fb7b395015f4
Patryk Banach <s29442@pjwstk.edu.pl>
2026-03-31 20:51:13 +0200

    Add CalculateAverage method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using apbd_cw1_git_s29442;
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+double avg = StatisticsHelper.CalculateAverage(numbers);
+Console.WriteLine($"Average: {avg}");
--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,6 +1,10 @@
 ﻿namespace apbd_cw1_git_s29442;
+using System.Linq;
 
 public static class StatisticsHelper
 {
-    
+    public static double CalculateAverage(int[] values)
+    {
+        return values.Average();
+    }
 }
