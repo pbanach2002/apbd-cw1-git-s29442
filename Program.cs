76db52fbb806a61667e2b5ff112fd0427637eaaa
Patryk Banach <s29442@pjwstk.edu.pl>
2026-03-31 20:51:39 +0200

    Add user input handling

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 ﻿using apbd_cw1_git_s29442;
 
-int[] numbers = { 1, 2, 3, 4, 5 };
+Console.WriteLine("Enter numbers separated by comma:");
+
+string input = Console.ReadLine();
+int[] numbers = input.Split(',')
+    .Select(x => int.Parse(x))
+    .ToArray();
 
 double avg = StatisticsHelper.CalculateAverage(numbers);
+
 Console.WriteLine($"Average: {avg}");
